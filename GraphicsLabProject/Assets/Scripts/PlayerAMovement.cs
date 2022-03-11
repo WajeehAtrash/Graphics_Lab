d4df0fbb0406f16727aac1f5c97e01fb0a3e7a0c
WajeehAtrash <35629900+WajeehAtrash@users.noreply.github.com>
2022-03-11 18:24:06 +0200

    fixing the jumping issue

--- GraphicsLabProject/Assets/Scripts/PlayerAMovement.cs
+++ GraphicsLabProject/Assets/Scripts/PlayerAMovement.cs
@@ -6,13 +6,17 @@ public class PlayerAMovement : MonoBehaviour
 {
     [SerializeField] private float speed=5f;
     [SerializeField] private float jumpforce = 5f;
+    [SerializeField]Transform groundCheck;
     private Rigidbody rBody;
     private float horizontalInput;
     private float verticalInput;
+    private CapsuleCollider capsuleCollider;
+    [SerializeField]private LayerMask groundLayer;
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
@@ -21,13 +25,19 @@ public class PlayerAMovement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         rBody.velocity = new Vector3(horizontalInput * speed, rBody.velocity.y, verticalInput * speed);
-        if(Input.GetKey(KeyCode.Space))
+     
+        if (Input.GetKey(KeyCode.Space)&&IsGrounded())
         {
             Jump();
         }
     }
-    void Jump()
+    private void Jump()
     {
         rBody.velocity = new Vector3(rBody.velocity.x, jumpforce, rBody.velocity.z);
+    }
+
+    private bool  IsGrounded()
+    {
+        return Physics.CheckSphere(groundCheck.position, 0.1f, groundLayer);
     }
 }
